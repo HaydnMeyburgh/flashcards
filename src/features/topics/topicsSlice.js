08b1cfebddc302f0e4fe31ea82fb75c685831bd0
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-25 12:32:11 +0200

    Changed the addTopic action to not use push method

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -9,14 +9,12 @@ export const topicsSlice = createSlice({
   reducers: {
     //action creator - this will add the topic object to the initial state topics object
     addTopic: (state, action) => {
-      state.topics.push({
-        [action.payload.id]: {
+        state.topics[action.payload.id] = {
         id: action.payload.id,
         name: action.payload.name,
         icon: action.payload.icon,
         quizIds: []
         }
-      })
     }
   }
 })
@@ -25,4 +23,3 @@ export const { addTopic } = topicsSlice.actions;
 export const selectTopic = (state) => state.topics.topics;
 export default topicsSlice.reducer;
 // console.log(topicsSlice);
-//add this page to github
